ec5afea2f8aa4a72226487f1d4634ae77dfc810d
Tyler Lowrey <contact@tylerlowrey.com>
2023-10-26 00:22:37 -0400

    Refactor destructuring of capture device capabilities

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ fn main() {
     let mut capture_device = Device::new(0)
         .expect("Failed to create a capture device. Make sure the camera is plugged in");
 
-    if let Ok(Capabilities { driver, card, bus, version, .. }) = capture_device.query_caps() {
-        println!("Camera info:\nDriver: {}\nCard: {}\nBus: {}\nVersion: {:?}\n",
-                 driver, card, bus, version);
-    }
+    let Ok(Capabilities { driver, card, bus, version, .. }) = capture_device.query_caps() else {
+        panic!("Unable to get capture device information.");
+    };
+
+    println!("Camera info:\nDriver: {}\nCard: {}\nBus: {}\nVersion: {:?}\n",
+             driver, card, bus, version);
 }
